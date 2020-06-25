620ec88e8c65533efd52cb90ee6f59022b1f07ce
Rajdeep jaiswal <57310771+rajdeepjaiswal@users.noreply.github.com>
2020-06-25 09:30:28 +0530

    Update ground.js

--- ground.js
+++ ground.js
@@ -11,7 +11,7 @@ class Ground{
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill("brown");
+      fill("blue");
     rect(pos.x, pos.y, this.w,this.h);
     }
   };
